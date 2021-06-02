6e130f53958ebc4aecaaef5d7a9be66b2c9b049f
g <gWOLF3@protonmail.com>
2021-06-01 21:23:15 -0500

    set board kernel to permissive mode AKA disable Selinux

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,3 +43,5 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/beryllium/BoardConfigVendor.mk
+
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
